72df0e198faf91255c0273c08cfc937cdf0df269
Alvaro Gabriel Gomez Andazol <alvarogabrielgomez@gmail.com>
2020-09-05 18:41:04 -0300

    ahora salva author

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
-    let response = {
-        title: "Test",
-        message: "mamaguevo"
+    res.send("Es necesario saber cual usuario es");
+})
+
+router.get('/:user', (req, res) => {
+    var data = {
+        username: null
     }
-    res.render('index', response);
+    switch (parseInt(req.params.user)) {
+        case 0:
+            data.username = "Alvaro"
+            break;
+        case 1:
+            data.username = "Angela"
+            break;
+        default:
+            res.send("Es necesario saber cual usuario es");
+            break;
+    }
+
+    res.render('index', data);
 })
 
 module.exports = router;
